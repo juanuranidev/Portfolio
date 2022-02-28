6cdcdb9e288d544a94569fa8cfaf41708eee6ede
Juan Urani <juanuranidev@gmail.com>
2022-02-27 23:22:30 -0300

    updates

---
+++ src/Contact/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './_Contact.scss';
+
+const Contact = () => {
+  return (
+    <section className='contact'>
+        <h2 className='contact_h2'>Hablemos un rato</h2>
+    </section>
+  )
+}
+
+export default Contact;
