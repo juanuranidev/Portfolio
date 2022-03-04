b97ad3cc060a3135147a00b593fb291b3b94edee
Juan Urani <juanuranidev@gmail.com>
2022-03-04 18:56:05 -0300

    updates

--- src/Pokeball/Pokeball.js
+++ src/Pokeball/Pokeball.js
@@ -1,24 +1,29 @@
 import React, { useState } from 'react';
-import PokeballImage from './img/pokeball.png'
+import PokeballImage from './img/pokeball.png';
 import './_Pokeball.scss';
 
 const Pokeball = () => {
 const [data, setData] = useState()
-const [data2, setData2] = useState(false)
 
   const handleGetPokemon = () => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * (90 - 0 + 1) + 0)}`)
     .then(resp => resp.json())
     .then(resp => setData(resp))
-    .finally(() => setData2(true))
   }
 
 
-  return (<>
-      {data2===true
-      ? <img src={data.sprites.front_shiny}/>
-      : <img src={PokeballImage} className='pokeball' onClick={() => handleGetPokemon()} />}
-    </>
+  return (
+    <div className='pokeball'>
+    {data
+    ? <div className='pokeball_div'>
+        <img src={data.sprites.front_shiny} className='pokeball_div_img' onClick={() => setData()} />
+        <p className='pokeball_div_p'>¡Encontraste un {data.name}!</p>
+      </div>
+    : <>
+        <img src={PokeballImage} className='pokeball_img' onClick={() => handleGetPokemon()} />   
+      </>
+    }
+  </div>
   )
 }
 
