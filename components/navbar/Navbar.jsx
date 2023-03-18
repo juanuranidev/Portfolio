483db6a3a73373d5148165862463d8b5fbf2f028
juanuranidev <juanuranidev@gmail.com>
2023-03-18 20:30:16 -0300

    Changed links in Navbar

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -44,7 +44,7 @@ export default function Navbar() {
               },
             }}
           >
-            Informacion
+            Experiencia
           </Button>
           <Button
             variant="link"
