efea7da00357c5ccaf86d2419f5bbd80f64e4be1
juanuranidev <juanuranidev@gmail.com>
2023-03-19 13:57:43 -0300

    Changed AnimatedComponent for Animation

--- views/github/GitHub.jsx
+++ views/github/GitHub.jsx
@@ -1,36 +1,36 @@
 import React from "react";
 import { Container, Heading, Img, Flex, Text } from "@chakra-ui/react";
-import AnimatedComponent from "../../components/animatedComponent/AnimatedComponent";
+import Animation from "../../components/animation/Animation";
 
 export default function GitHub() {
   return (
     <Container
-      my="20"
+      my="40"
       maxW="container.xl"
       display="flex"
       flexDirection="column"
       alignItems="center"
     >
-      <Heading mb="20">GITHUB</Heading>
+      <Heading mb="40">GITHUB</Heading>
       <Flex mb="10">
-        <AnimatedComponent>
+        <Animation>
           <Img
             src="https://github-readme-stats.vercel.app/api?username=juanuranidev&&show_icons=true&theme=dark&text_color=ffffff&count_private=true&include_all_commits=true&locale=en"
             mr="10"
             w="30rem"
           />
-        </AnimatedComponent>
-        <AnimatedComponent>
+        </Animation>
+        <Animation>
           <Img
             src="https://github-readme-stats.vercel.app/api/top-langs/?username=juanuranidev&layout=compact&theme=dark&text_color=ffffff&locale=es"
             w="22.9rem"
           />
-        </AnimatedComponent>
+        </Animation>
       </Flex>
-      <AnimatedComponent>
+      <Animation>
         <Img src="http://ghchart.rshah.org/juanuranidev" w="50rem" />
         <Text fontWeight="700">Commits 2022 - 2023</Text>
-      </AnimatedComponent>
+      </Animation>
     </Container>
   );
 }
--- views/projects/Projects.jsx
+++ views/projects/Projects.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Heading } from "@chakra-ui/react";
-import AnimatedComponent from "../../components/animatedComponent/AnimatedComponent";
+import Animation from "../../components/animation/Animation";
 import Project1 from "../../assets/images/Project1.png";
 import Project2 from "../../assets/images/Project2.png";
 import Project from "../../components/project/Project";
@@ -8,14 +8,14 @@ import Project from "../../components/project/Project";
 export default function Projects() {
   return (
     <Container
-      my="20"
+      mt="40"
       maxW="container.xl"
       display="flex"
       flexDirection="column"
       alignItems="center"
     >
-      <Heading mb="20">PROYECTOS</Heading>
-      <AnimatedComponent>
+      <Heading mb="40">PROYECTOS</Heading>
+      <Animation>
         <Project
           title="Frères"
           description="Frères es una tienda online que vende ropa que siempre me ha gustado, pensé que podría tener un mejor diseño así que me propuse crearlo."
@@ -24,8 +24,8 @@ export default function Projects() {
           repository={"https://github.com/juanuranidev/Freres"}
           website={"https://freres.netlify.app/"}
         />
-      </AnimatedComponent>
-      <AnimatedComponent>
+      </Animation>
+      <Animation>
         <Project
           title="Entrenally"
           description="Aplicación de autogestión para ayudar a entrenadores a realizar las tareas relacionadas a su trabajo en un solo lugar."
@@ -35,7 +35,7 @@ export default function Projects() {
           website={"https://entrenally.vercel.app/"}
           hasRightImage
         />
-      </AnimatedComponent>
+      </Animation>
     </Container>
   );
 }
