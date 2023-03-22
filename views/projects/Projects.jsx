9dd36569d02e1b9bc30fcc53045b475f772ec7ca
juanuranidev <juanuranidev@gmail.com>
2023-03-22 00:31:19 -0300

    Changed projects description

--- views/projects/Projects.jsx
+++ views/projects/Projects.jsx
@@ -23,7 +23,7 @@ export default function Projects() {
       <Animation>
         <Project
           title="Frères"
-          description="Frères es una tienda online que vende ropa que siempre me ha gustado, pensé que podría tener un mejor diseño así que me propuse crearlo."
+          description="Frères es una marca de ropa existente cuyos productos siempre me han gustado, pensé que su página web podía tener un mejor diseño así que me propuse a mejorarla."
           technologies={["React.js", "TypeScript", "SASS", "Firebase"]}
           image={Project1.src}
           repository={"https://github.com/juanuranidev/Freres"}
@@ -33,7 +33,7 @@ export default function Projects() {
       <Animation>
         <Project
           title="Entrenally"
-          description="Aplicación de autogestión para ayudar a entrenadores a realizar todas las tareas relacionadas a su trabajo en un solo lugar y de manera sencilla."
+          description="Entrenally es una aplicación de autogestión para ayudar a entrenadores a realizar las tareas relacionadas a su trabajo en un solo lugar y de manera sencilla."
           technologies={["Next.js", "JavaScript", "Chakra-ui"]}
           image={Project2.src}
           repository={""}
