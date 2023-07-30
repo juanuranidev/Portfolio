0617c3d67ad02bcf11ade500668f5f06ad9f21aa
juanuranidev <juanuranidev@gmail.com>
2023-07-30 04:35:36 -0300

    Added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { I18nextProvider } from "react-i18next";
 import { LanguageProvider } from "../context/LanguageContext";
@@ -10,6 +11,7 @@ function MyApp({ Component, pageProps }) {
       <LanguageProvider>
         <I18nextProvider i18n={i18n}>
           <Component {...pageProps} />
+          <Analytics />
         </I18nextProvider>
       </LanguageProvider>
     </ChakraProvider>
