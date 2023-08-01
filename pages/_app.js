e185c6183ff1bc8f62a876375bca110df3bc393e
juanuranidev <juanuranidev@gmail.com>
2023-07-31 23:59:25 -0300

    Updated assets folder

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,9 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { I18nextProvider } from "react-i18next";
 import { LanguageProvider } from "../context/LanguageContext";
 import i18n from "../config/reactI18Next";
-import "../styles/globals.css";
+import "../assets/styles/globals.css";
+import "../assets/styles/animations.css";
+import "../assets/styles/keyframes.css";
 
 function MyApp({ Component, pageProps }) {
   return (
